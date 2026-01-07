6e6da9b127f46bc26213c1955e6372a6edaef6ce
diego4lbarracin <diegoalbarracin0405@gmail.com>
2026-01-07 12:19:52 -0500

    Default message.

--- backend/Go_API/main.go
+++ backend/Go_API/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"example/Go_API/handlers"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,6 +10,15 @@ import (
 func main(){
 	router := gin.Default()
 
+	router.GET("/", func(c *gin.Context) {
+        c.JSON(http.StatusOK, gin.H{
+            "message": "Welcome to Compound Interest Calculator API",
+            "endpoints": gin.H{
+                "calculation": "/calculation",
+            },
+        })
+    })
+	
 	router.GET("/calculation", handlers.GetCalculation)
 
 	router.Run(":3536")
